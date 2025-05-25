3fd83770f6c8f811ee42ba18f198d539e8e94920
omniaosman <UGS.140547@ci.suez.edu.eg>
2025-05-25 14:08:15 +0300

    fix: github actions vars

--- core/settings.py
+++ core/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME') or os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('DB_USER') or os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD') or os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST') or os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('DB_PORT') or os.getenv('POSTGRES_PORT'),
     }
 }
 
